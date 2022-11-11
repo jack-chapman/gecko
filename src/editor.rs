b417e602960864f519de0b240bbab77784823507
jack-chapman <jackchapman093@gmail.com>
2022-11-11 21:34:56 +0000

    Add ctrl s to save to disk

--- src/editor.rs
+++ src/editor.rs
@@ -101,6 +101,14 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Ctrl('s') => {
+                if self.document.save().is_ok() {
+                    self.status_message =
+                        StatusMessage::from("File saved successfully".to_string());
+                } else {
+                    self.status_message = StatusMessage::from("Error writing file!".to_string());
+                }
+            }
             Key::Char(c) => {
                 self.document.insert(&self.cursor_position, c);
                 self.move_cursor(Key::Right);
